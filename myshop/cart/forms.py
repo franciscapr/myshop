b0ffeccfa50ee67fba3a3a34cfbbe84ea1523f73
Francisca <francisca.paredes.pr@gmail.com>
2024-12-09 11:36:38 -0300

    translate base.html

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 # Utilizaremos este form para añadir productos al carrito
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(    # Permite al usuario seleccionar la cantidad de 1 a 20
         choices=PRODUCT_QUANTITY_CHOICES,
-        coerce=int    # Convertimos la entrada en un número entero
+        coerce=int,    # Convertimos la entrada en un número entero
+        label=_('Quantity')
     )
     override = forms.BooleanField(    # Indica si la cantidad debe sumarse a caulquier cantidad existente en el cart False
         required=False,               # O si la cantidad existente debe ser reemplazada por la cantidad dada True
--- myshop/coupons/forms.py
+++ myshop/coupons/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
+
 
 # Formulario que utilizaremos para que el usuario ingrese el cupon de descuento
 class CouponApplyForm(forms.Form):
-    code = forms.CharField()
+    code = forms.CharField(lable=_('Coupon'))
 
     
 
