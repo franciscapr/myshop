fca48f6b1e680fa341838d85e3e5efbdd701f4a6
Francisca <francisca.paredes.pr@gmail.com>
2024-12-09 13:55:47 -0300

    url i18n_patterns

--- myshop/coupons/forms.py
+++ myshop/coupons/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Formulario que utilizaremos para que el usuario ingrese el cupon de descuento
 class CouponApplyForm(forms.Form):
-    code = forms.CharField(lable=_('Coupon'))
+    code = forms.CharField(label=_('Coupon'))
 
     
 
