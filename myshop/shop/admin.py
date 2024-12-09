9ffcaba7f35267d0adb70b1509cdb7597ef2f3d4
Francisca <francisca.paredes.pr@gmail.com>
2024-12-09 14:46:32 -0300

    TranslatableAdmin

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
-
+from parler.admin import TranslatableAdmin
 from .models import Category, Product
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
     # Utilizamos el propopulated para especificar los campos cuyo valor se establecen automáticamente usando el valor
     # de otros campos
     
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = [
         'name',
         'slug',
@@ -22,4 +24,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']    # Utilizamos para establecer los campos que se pueden editar desde lapágina 
     # de visualización de la lista en el sitio de administración.
-    prepopulated_fields = { 'slug': ('name',)}
+    def get_prepopulated_fields(self, request, obj=None):
+        return { 'slug': ('name',)}
