6f7b15a0ee595ed7fabdafc4f6b641b663370262
Francisca <francisca.paredes.pr@gmail.com>
2024-12-09 15:51:03 -0300

    postal_code

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -9,7 +9,12 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)    # Recuperamos solo los productos disponibles
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
+        language = request.LANGUAGE_CODE
+        category = get_object_or_404(
+            Category,
+            translations__language_code=language,
+            translations__slug=category_slug,
+            )
         products = products.filter(category=category)
     return render(
         request,
@@ -23,8 +28,13 @@ def product_list(request, category_slug=None):
     
 # *** DETALLES DEL PRODUCTO ***
 def product_detail(request, id, slug):
+    language = request.LANGUAGE_CODE
     product = get_object_or_404(
-        Product, id=id, slug=slug, available=True    # Se esperan los parámetros id y slug para recuperar la instancia de Product
+        Product,
+        id=id,
+        translations__language_code=language,
+        translations__slug=slug,
+        available=True    # Se esperan los parámetros id y slug para recuperar la instancia de Product
     )
     cart_product_form = CartAddProductForm()
     r = Recommender()
